d4b09f95525fae0f0f75cb007b7c65c3fa3522c1
Charles Floyd <charleswfloyd3@gmail.com>
2021-12-16 19:37:06 +0200

    debugging react hook form package

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import coins from './userParameters/coins.json'
 import { Routes, Route}  from 'react-router-dom';
 // import UserParameters from './userParameters/userParameters';
 import Navbar from './navbar/navbar';
-import background from './2.webp'
-import lightbackground from './bc.jpg'
 import Info from './info/info';
 
 function App() {
   let color = "white"
   return (
     <Routes>
-        <Route exact path="/" element={<div className="App" style={{backgroundImage: `url(${background})` }}>
+        <Route exact path="/" element={<div className="App"  >
           <Navbar colordata={color} navleft={"Learn More"}/>
           <SearchBar placeholder="Enter a cryptocurrency..." data={coins}   />
           {/* <p className="footer">© Cryptobase All rights reserved. 2021</p> */}
